9f388e877ee1e655b1962ed626b8208626ad146d
Olivia Stodieck <55671906+ostodieck@users.noreply.github.com>
2024-05-12 10:52:48 +0100

    resolve jox config bug

--- fem4inas/fem4inas_main.py
+++ fem4inas/fem4inas_main.py
@@ -3,7 +3,11 @@ import fem4inas.drivers
 import fem4inas.preprocessor.configuration as configuration  # Config, ValidateConfig
 from fem4inas.drivers.driver import Driver
 from fem4inas.preprocessor.solution import Solution
-from jax.config import config; config.update("jax_enable_x64", True)
+import jax
+
+config = jax.config
+config.update("jax_enable_x64", True)
+
 
 def main(input_file: str = None,
          input_dict: dict = None,
